ea124855ce6f548108e577be136ca5a15af1dbd7
Marco Almeida <marco.almeida.us@gmail.com>
2016-12-20 15:02:34 -0500

    new

---
+++ TableTableViewController.swift
@@ -0,0 +1,97 @@
+//
+//  TableTableViewController.swift
+//  LoadJSon
+//
+//  Created by Marco Almeida on 12/19/16.
+//  Copyright © 2016 THE IRON YARD. All rights reserved.
+//
+
+import UIKit
+
+class TableTableViewController: UITableViewController {
+
+    struct Team {
+        var category = ""
+        var name = ""
+        var url = ""
+    }
+    var aTeamArray = [Team]()    // initialize an empty Array
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+//        do { try.... } catch { let nserror = error as NSError print("Details of JSON parsing error:\n \(nserror)")}
+        
+        let path = Bundle.main.path(forResource: "sportTeams",ofType:"json")
+        if let binaryData = NSData(contentsOfFile: path!) {
+        do {
+            let arrayOfDictionaries = try JSONSerialization.jsonObject(with: binaryData as Data, options: .allowFragments) as! [[String: Any]]
+             // an Array of Dictionaries
+
+            for aDictionary in arrayOfDictionaries {
+                let NameT = aDictionary["name"] as! String
+                let urlT = aDictionary["url"] as! String
+                let categoryT = aDictionary["category"] as! String
+                let anTeam = Team(category: categoryT, name: NameT, url: urlT)
+                aTeamArray.append(anTeam)
+            }
+          }
+          //else throw an error detailing what went wrong
+          catch let error as NSError {
+          print("Details of JSON parsing error:\n \(error)")
+          }
+        }
+        
+//print(aTeamArray.count)
+
+        
+    } // end of viewDidLoad
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return aTeamArray.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DetailCell", for: indexPath)
+
+        let aTeam = aTeamArray[indexPath.row]
+        cell.textLabel?.text = aTeam.name
+        cell.detailTextLabel?.text = aTeam.url
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        _ = tableView.cellForRow(at: indexPath as IndexPath) as UITableViewCell!
+        
+        
+        // launch WebLink with the currentCell
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+ 
+}
